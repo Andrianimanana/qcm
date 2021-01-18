85319f7fe1466772a4e386773eb9b5c72e909270
Armel <arbandry@gmail.com>
2021-01-18 13:01:35 +0300

    hide field index_question

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Question;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\HiddenField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -21,7 +22,7 @@ class QuestionCrudController extends AbstractCrudController
     {
         return [ 
             TextField::new('libele'),
-            IntegerField::new('index_question'),
+            HiddenField::new('index_question'),
             NumberField::new('countdown')
                 ->setLabel('Durée (en séconde)')
                 ->setRoundingMode(2),
